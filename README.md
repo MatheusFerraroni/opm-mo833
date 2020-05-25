17f50893eff8faeef64cc32773b8a30805aa8278
Matheus <matheussanches531@gmail.com>
2020-05-25 16:17:44 -0300

    nd

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# opm-mo833
+## Informações básicas
+
+- Aluno: Matheus Ferraroni Sanches
+- RA: 212142
+- Data: maio/2020
+- Objetivo: Avaliar <a href="https://opm-project.org/">OPEN POROUS MEDIA (OPM)</a>.
+
+## Instâncias
+
+Tipo da instância: t2.micro
+
+AMI: ami-039a49e70ea773ffc
+
+Security group: 
---
+++ scripts/setup1.sh
@@ -0,0 +1,32 @@
+
+echo "iniciando" > ~/log_geral.txt
+
+sudo apt-get update
+
+echo "update ok" >> ~/log_geral.txt
+
+# sudo apt-get install -y wget make gcc libgfortran3 \
+# sysstat libibnetdisc-dev openmpi-bin libopenmpi-dev \
+# libhdf5-openmpi-dev gfortran build-essential git
+
+# echo "apt-get install ok" >> ~/log_geral.txt
+
+# wget https://github.com/Kitware/CMake/releases/download/v3.17.2/cmake-3.17.2-Linux-x86_64.sh
+
+
+# echo "pegou instalador do cmake" >> ~/log_geral.txt
+
+
+# chmod +x cmake-3.17.2-Linux-x86_64.sh
+# echo "permissao no arquivo para instalar cmake" >> ~/log_geral.txt
+# bash cmake-3.17.2-Linux-x86_64.sh --skip-license
+# echo "instalou cmake" >> ~/log_geral.txt
+# sudo ln -s /home/ubuntu/bin/* /usr/local/bin
+# echo "criou link cmake" >> ~/log_geral.txt
+
+
+
+
+
+
+echo "all complete" >> ~/log_geral.txt
