83b1476a5f4213148d95dee3013d7a918dced983
Matheus <matheussanches531@gmail.com>
2020-05-26 17:18:42 -0300

    nd

--- scripts/setup2.sh
+++ scripts/setup2.sh
@@ -1,11 +1,8 @@
 
-
-
 cd opm-common/build
 cmake .. 1> cmake.out 2> cmake.outerr
 make -j 2 1> make.out 2> make.outerr
 cd ~
-
 
 cd opm-material/build
 cmake .. 1> cmake.out 2> cmake.outerr
@@ -24,11 +21,10 @@ cd ~
 
 cd opm-simulators/build
 cmake .. 1> cmake.out 2> cmake.outerr
-make -j 2 1> make.out 2> make.outerr
+make 1> make.out 2> make.outerr
 cd ~
 
 cd opm-upscaling/build
 cmake .. 1> cmake.out 2> cmake.outerr
 make -j 2 1> make.out 2> make.outerr
 cd ~
-
