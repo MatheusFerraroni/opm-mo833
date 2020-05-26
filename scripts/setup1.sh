551908c6ff5e5aa38e316659464a223a8a9f0dc5
Matheus <matheussanches531@gmail.com>
2020-05-26 09:49:45 -0300

    nd

--- scripts/setup1.sh
+++ scripts/setup1.sh
@@ -29,34 +29,34 @@ echo "update ok" >> ~/log_geral.txt
 
 
 
-# Make sure we have updated URLs to packages etc.
-sudo apt-get update -y
+# # Make sure we have updated URLs to packages etc.
+# sudo apt-get update -y
 
-# For server edition of Ubuntu add-apt-repository depends on
-sudo apt-get install -y software-properties-common
+# # For server edition of Ubuntu add-apt-repository depends on
+# sudo apt-get install -y software-properties-common
 
-# Add PPA for OPM packages
-sudo add-apt-repository -y ppa:opm/ppa
-sudo apt-get update -y
+# # Add PPA for OPM packages
+# sudo add-apt-repository -y ppa:opm/ppa
+# sudo apt-get update -y
 
-# Packages necessary for building
-sudo apt-get install -y build-essential gfortran pkg-config cmake
+# # Packages necessary for building
+# sudo apt-get install -y build-essential gfortran pkg-config cmake
 
-# Packages necessary for documentation
-sudo apt-get install -y doxygen ghostscript texlive-latex-recommended pgf gnuplot
+# # Packages necessary for documentation
+# sudo apt-get install -y doxygen ghostscript texlive-latex-recommended pgf gnuplot
 
-# Packages necessary for version control
-sudo apt-get install -y git-core
+# # Packages necessary for version control
+# sudo apt-get install -y git-core
 
-# MPI for parallel programs
-sudo apt-get install -y mpi-default-dev
-# Prerequisite libraries
-sudo apt-get install -y libblas-dev libboost-all-dev \
-  libsuitesparse-dev libtrilinos-zoltan-dev
+# # MPI for parallel programs
+# sudo apt-get install -y mpi-default-dev
+# # Prerequisite libraries
+# sudo apt-get install -y libblas-dev libboost-all-dev \
+#   libsuitesparse-dev libtrilinos-zoltan-dev
 
-# Parts of Dune needed
-sudo apt-get install libdune-common-dev libdune-geometry-dev \
-  libdune-istl-dev libdune-grid-dev
+# # Parts of Dune needed
+# sudo apt-get install libdune-common-dev libdune-geometry-dev \
+#   libdune-istl-dev libdune-grid-dev
 
 
 
