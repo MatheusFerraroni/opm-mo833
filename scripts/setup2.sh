ed78a12a62efe1dfeb703bb94e30263de9cde816
Matheus <matheussanches531@gmail.com>
2020-05-27 15:31:50 -0300

    nd

--- scripts/setup2.sh
+++ scripts/setup2.sh
@@ -3,41 +3,41 @@ echo "INICIANDO COMPILERS" >> ~/log_geral.txt
 
 cd ~/opm-common/build
 cmake .. -DUSE_MPI=1 1> cmake.out 2> cmake.outerr
-make -i -j 2  1> make_res.out 2> make_res.outerr
+make -i -j  1> make_res.out 2> make_res.outerr
 
 
 echo "COMPILERS 1" >> ~/log_geral.txt
 
 cd ~/opm-material/build
 cmake .. -DUSE_MPI=1 1> cmake.out 2> cmake.outerr
-make -i -j 2  1> make_res.out 2> make_res.outerr
+make -i -j  1> make_res.out 2> make_res.outerr
 
 
 echo "COMPILERS 2" >> ~/log_geral.txt
 
 cd ~/opm-grid/build
 cmake .. -DUSE_MPI=1 1> cmake.out 2> cmake.outerr
-make -i -j 2  1> make_res.out 2> make_res.outerr
+make -i -j  1> make_res.out 2> make_res.outerr
 
 
 echo "COMPILERS 3" >> ~/log_geral.txt
 
 cd ~/opm-models/build
 cmake .. -DUSE_MPI=1 1> cmake.out 2> cmake.outerr
-make -i -j 2  1> make_res.out 2> make_res.outerr
+make -i -j  1> make_res.out 2> make_res.outerr
 
 
 echo "COMPILERS 4" >> ~/log_geral.txt
 
 cd ~/opm-simulators/build
 cmake .. -DUSE_MPI=1 1> cmake.out 2> cmake.outerr
-make -i -j 2  1> make_res.out 2> make_res.outerr
+make -i -j  1> make_res.out 2> make_res.outerr
 
 
 echo "COMPILERS 5" >> ~/log_geral.txt
 
 cd ~/opm-upscaling/build
 cmake .. -DUSE_MPI=1 1> cmake.out 2> cmake.outerr
-make -i -j 2  1> make_res.out 2> make_res.outerr
+make -i -j  1> make_res.out 2> make_res.outerr
 
 echo "COMPILERS COMPLETE" >> ~/log_geral.txt
