2c574ac2c5d0d3c99c23e06157a2a7360e713770
Matheus <matheussanches531@gmail.com>
2020-05-26 23:09:52 -0300

    nd

--- scripts/setup2.sh
+++ scripts/setup2.sh
@@ -1,30 +1,30 @@
 
 cd opm-common/build
 cmake .. -DUSE_MPI=1
-make -i -j
+make -i
 cd ~
 
 cd opm-material/build
 cmake .. -DUSE_MPI=1
-make -i -j
+make -i
 cd ~
 
 cd opm-grid/build
 cmake .. -DUSE_MPI=1
-make -i -j
+make -i
 cd ~
 
 cd opm-models/build
 cmake .. -DUSE_MPI=1
-make -i -j
+make -i
 cd ~
 
 cd opm-simulators/build
 cmake .. -DUSE_MPI=1
-make -i -j
+make -i
 cd ~
 
 cd opm-upscaling/build
 cmake .. -DUSE_MPI=1
-make -i -j
+make -i
 cd ~
